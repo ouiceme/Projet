9dbc13532e43ad3d4678cc117dc4126a2b67c455
Ouiceme <ouiceme.b@gmail.com>
2019-05-29 16:28:15 +0200

    mise à jour pour travis - modif install package

--- tools/deploy.sh
+++ tools/deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -e
 cd $TRAVIS_BUILD_DIR/packer
-packer build ____________MYPACKERFILE_________ | tee /tmp/packer.out
+packer build webapp.json | tee /tmp/packer.out
 AMI=$(awk -F':' '/(eu|us|ap|sa)-(west|central|east|northeast|southeast)-(1|2): ami-/ {print $2}' /tmp/packer.out |  tr -d '[[:space:]]')
 
 cd $TRAVIS_BUILD_DIR/webapp
